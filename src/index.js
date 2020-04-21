c2fcecf0e2697a47558aa6907c932116f6c8315e
Nay Mello <naymellofilho@gmail.com>
2020-04-21 13:39:39 -0300

    Delete serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
