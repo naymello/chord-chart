30d8a601af17796df1c2fadf3dcd0f91a0307fa3
Nay Mello <naymellofilho@gmail.com>
2020-04-22 17:09:01 -0300

    Apply 'Poppins' font family to all elements

--- src/styles.js
+++ src/styles.js
@@ -3,9 +3,12 @@ import styled, { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
+  * {
+    font-family: 'Poppins', sans-serif;
+  }
+
   body {
     margin: 0;
-    font-family: 'Poppins', sans-serif;
   }
 `;
 
