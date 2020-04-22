c029f2700f8b5da5a2160b7f0bbd2431a4d27dbf
Nay Mello <naymellofilho@gmail.com>
2020-04-22 15:03:23 -0300

    Set 'S.' naming convention to styled components
    See: https://medium.com/inturn-eng/naming-styled-components-d7097950a245

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import logo from '../assets/logo.png'
-import { Logo, Nav } from '../styles'
+import * as S from '../styles'
 
 export default function Navbar() {
   return (
-    <Nav>
-      <Logo src={logo} alt="ChordChart" />
-    </Nav>
+    <S.Nav>
+      <S.Logo src={logo} alt="ChordChart" />
+    </S.Nav>
   );
 }
