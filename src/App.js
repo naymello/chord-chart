a22e91bcf968b35f765ad4c230b437b2b53cdd7c
Nay Mello <naymellofilho@gmail.com>
2020-04-24 14:56:21 -0300

    Delete Notes component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { GlobalStyle } from './styles';
 import Navbar from './components/Navbar';
 import Select from './components/Select';
-import Notes from './components/Notes';
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
       <GlobalStyle />
       <Navbar />
       <Select />
-      <Notes />
     </React.Fragment>
   );
 }
