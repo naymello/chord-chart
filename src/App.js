eac0b30cf663ea2ed3c3385264526e871669bad9
Nay Mello <naymellofilho@gmail.com>
2020-04-24 15:03:38 -0300

    Create Note component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import * as S from './styles';
 import Navbar from './components/Navbar';
 import Select from './components/Select';
+import Note from './components/Note';
 
 export default function App() {
   return (
@@ -10,6 +11,20 @@ export default function App() {
       <S.GlobalStyle />
       <Navbar />
       <Select />
+      <S.Notes>
+        <Note note="C" />
+        <Note note="C#" />
+        <Note note="D" />
+        <Note note="D#" />
+        <Note note="E" />
+        <Note note="F" />
+        <Note note="F#" />
+        <Note note="G" />
+        <Note note="G#" />
+        <Note note="A" />
+        <Note note="A#" />
+        <Note note="B" />
+      </S.Notes>
     </React.Fragment>
   );
 }
