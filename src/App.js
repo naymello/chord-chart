c5f1ad98c113a3ee22b5970a8386d0fc2ddbe90b
Nay Mello <naymellofilho@gmail.com>
2020-04-27 13:58:51 -0300

    Change App component to ES6 'class' syntax

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import * as S from './styles';
 import Navbar from './components/Navbar';
 import Select from './components/Select';
 import Note from './components/Note';
 
-export default function App() {
-  return (
-    <React.Fragment>
-      <S.GlobalStyle />
-      <Navbar />
-      <Select />
-      <S.Notes>
-        <Note note="C" />
-        <Note note="C#" />
-        <Note note="D" />
-        <Note note="D#" />
-        <Note note="E" />
-        <Note note="F" />
-        <Note note="F#" />
-        <Note note="G" />
-        <Note note="G#" />
-        <Note note="A" />
-        <Note note="A#" />
-        <Note note="B" />
-      </S.Notes>
-    </React.Fragment>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <React.Fragment>
+        <S.GlobalStyle />
+        <Navbar />
+        <Select />
+        <S.Notes>
+          <Note note="C" />
+          <Note note="C#" />
+          <Note note="D" />
+          <Note note="D#" />
+          <Note note="E" />
+          <Note note="F" />
+          <Note note="F#" />
+          <Note note="G" />
+          <Note note="G#" />
+          <Note note="A" />
+          <Note note="A#" />
+          <Note note="B" />
+        </S.Notes>
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
