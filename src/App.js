2a42bb7b5ff789d43bd420a80eb53b6c00d05d8b
Nay Mello <naymellofilho@gmail.com>
2020-04-28 15:41:51 -0300

    Delete notes from App state

--- src/App.js
+++ src/App.js
@@ -10,20 +10,6 @@ class App extends Component {
     super(props);
 
     this.state = {
-      notes: [
-        { id: 1, name: 'C' },
-        { id: 2, name: 'C#' },
-        { id: 3, name: 'D' },
-        { id: 4, name: 'D#' },
-        { id: 5, name: 'E' },
-        { id: 6, name: 'F' },
-        { id: 7, name: 'F#' },
-        { id: 8, name: 'G' },
-        { id: 9, name: 'G#' },
-        { id: 10, name: 'A' },
-        { id: 11, name: 'A#' },
-        { id: 12, name: 'B' }
-      ]
     }
   }
 
