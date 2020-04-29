4c68f322cbcec6867da33a7e732f288f36040fd1
Nay Mello <naymellofilho@gmail.com>
2020-04-29 17:35:41 -0300

    Move notes state property to Notes component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,10 +3,31 @@ import React, { Component } from 'react';
 import * as S from '../styles';
 
 export class Notes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, name: 'C' },
+        { id: 2, name: 'C#' },
+        { id: 3, name: 'D' },
+        { id: 4, name: 'D#' },
+        { id: 5, name: 'E' },
+        { id: 6, name: 'F' },
+        { id: 7, name: 'F#' },
+        { id: 8, name: 'G' },
+        { id: 9, name: 'G#' },
+        { id: 10, name: 'A' },
+        { id: 11, name: 'A#' },
+        { id: 12, name: 'B' }
+      ]
+    }
+  }
+
   render() {
     return (
       <S.Chords>
-        {this.props.notes.map(note => {
+        {this.state.notes.map(note => {
           return (
             <div key={note.id}>
               <input
