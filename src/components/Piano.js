aca94df58b5caabed1b5783f6e82ea9027c15c35
Nay Mello <naymellofilho@gmail.com>
2020-05-11 16:07:49 -0300

    Add 'note' property to pianoNotes array

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -4,19 +4,21 @@ import { SvgLoader, SvgProxy } from 'react-svgmt';
 import piano from '../assets/piano.svg';
 
 export class Piano extends Component {
+  //Only notes from the first octave of the piano have a 'note' property.
+  //That's because they are the only ones that will be used as roots for the chords.
   pianoNotes = [
-    { id: 1, name: 'C1' },
-    { id: 2, name: 'C-sharp1' },
-    { id: 3, name: 'D1' },
-    { id: 4, name: 'D-sharp1' },
-    { id: 5, name: 'E1' },
-    { id: 6, name: 'F1' },
-    { id: 7, name: 'F-sharp1' },
-    { id: 8, name: 'G1' },
-    { id: 9, name: 'G-sharp1' },
-    { id: 10, name: 'A1' },
-    { id: 11, name: 'A-sharp1' },
-    { id: 12, name: 'B1' },
+    { id: 1, name: 'C1', note: 'C' },
+    { id: 2, name: 'C-sharp1', note: 'C#' },
+    { id: 3, name: 'D1', note: 'D' },
+    { id: 4, name: 'D-sharp1', note: 'D#' },
+    { id: 5, name: 'E1', note: 'E' },
+    { id: 6, name: 'F1', note: 'F' },
+    { id: 7, name: 'F-sharp1', note: 'F#' },
+    { id: 8, name: 'G1', note: 'G' },
+    { id: 9, name: 'G-sharp1', note: 'G#' },
+    { id: 10, name: 'A1', note: 'A' },
+    { id: 11, name: 'A-sharp1', note: 'A#' },
+    { id: 12, name: 'B1', note: 'B' },
     { id: 13, name: 'C2' },
     { id: 14, name: 'C-sharp2' },
     { id: 15, name: 'D2' },
